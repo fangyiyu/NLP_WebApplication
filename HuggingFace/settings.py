e4c6df408c97fcf02dccc2b1323ebeffa36c162e
Fangyi Yu <yufangyi1@gmail.com>
2021-06-19 19:30:03 -0400

    added procfile and requirements

--- HuggingFace/settings.py
+++ HuggingFace/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'NAME.herokuapp.com']
+ALLOWED_HOSTS = ['nlpelaine.herokuapp.com']
 
 
 # Application definition
