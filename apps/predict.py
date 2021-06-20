5aa5375d482d76adb50342fdd0d443d2c0f453b9
Fangyi Yu <yufangyi1@gmail.com>
2021-06-19 20:15:50 -0400

    added procfile and requirements

--- apps/predict.py
+++ apps/predict.py
@@ -1,25 +1,15 @@
 from transformers import AutoModelForMaskedLM, AutoTokenizer
-import torch
 from transformers import pipeline, set_seed
 
 
 
 def pred(a, b):
-  tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased")
-  model = AutoModelForMaskedLM.from_pretrained("distilbert-base-cased")
-  sequence = f"{a} {tokenizer.mask_token} {b}"
-
-  input = tokenizer.encode(sequence, return_tensors="pt")
-  mask_token_index = torch.where(input == tokenizer.mask_token_id)[1]
-
-  token_logits = model(input).logits
-  mask_token_logits = token_logits[0, mask_token_index, :]
-
-  top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
-  
+  nlp = pipeline("fill-mask", model='distilbert-base-cased')
+  sequence = f"{a} {nlp.tokenizer.mask_token} {b}"
+  result0 = nlp(sequence)
   result = []
-  for token in top_5_tokens:
-    result.append(tokenizer.decode([token]))
+  for i in range(len(result0)):
+    result.append(result0[i]['sequence'])
   return result
 
 def summarizetext(text):
